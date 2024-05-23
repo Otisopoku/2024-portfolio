fe71de115b2880903c2d476498d71ee5b043f239
Otis Opoku <oto226@lehigh.edu>
2024-05-22 22:50:47 -0700

    set up components

---
+++ backend_sanity/schemaTypes/index.ts
@@ -0,0 +1,17 @@
+import testimonials from "./testimonials";
+import about from "./about";
+import brands from "./brands";
+import skills from "./skills";
+import workExperience from "./workExperience";
+import works from "./works";
+import contacts from "./contacts";
+
+export const schemaTypes = [
+    testimonials,
+    about,
+    brands,
+    skills,
+    workExperience,
+    works,
+    contacts
+]
---
+++ frontend/README.md
@@ -0,0 +1,3 @@
+# Dependencies
+
+`npm install` @sanity/client @sanity/image-url framer-motion (for animations) -D sass
