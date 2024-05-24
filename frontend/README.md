c6c8f129660b183abefa8f7d37de421c2467e9a1
Otis Opoku <otisopoku30@gmail.com>
2024-05-24 12:52:28 -0700

    add and style navbar

--- frontend/README.md
+++ frontend/README.md
@@ -1,3 +1,7 @@
 # Dependencies
 
 `npm install` @sanity/client @sanity/image-url framer-motion (for animations) -D sass
+
+# Note
+
+scss enables us to do nesting of elements when styling as seen in the styles
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,10 +3,18 @@ import {images } from "../../constants"
 import './Navbar.scss';
 const Navbar = () => {
   return (
-    <nav>
-      <div>
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
         <img src={images.logo} alt="logo"/>
       </div>
+      <ul className='app__navbar-links'>
+        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+          <li key ={`link-${item}`} className='app__flex p-text'>
+            <div></div>
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
   )
 }
